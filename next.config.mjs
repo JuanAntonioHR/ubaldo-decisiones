b61e2d2e7490db04fb71d44a500a9fba24ebe661
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2024-08-22 16:21:30 -0600

    chore: Update npm dependencies and add Bootstrap and Bootswatch
    ```

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,49 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'dummyimage.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'sanluispotosi.quadratin.com.mx',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images-na.ssl-images-amazon.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.blogs.es',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.caritas.org.mx',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i0.wp.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'mapsicologos.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'media.semprefamilia.com.br',
+                port: '',
+            },
+        ]
+    },
+};
 
 export default nextConfig;
